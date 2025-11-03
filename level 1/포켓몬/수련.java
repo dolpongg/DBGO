58665927a522992c1b68f944ce36bd486ffee952
이수련 <dolpong_@naver.com>
2025-11-03 21:48:29 +0900

    수련 - 11/3 폰켓몬 완

---
+++ level 1/포켓몬/수련.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i : nums){
+            set.add(i);
+        }
+
+        if(set.size() > nums.length/2) return nums.length/2;
+        else return set.size();
+    }
+}

8a5c76cc06acf0f95f5d829aab1397c02119378f
daykim <45174177+ekdud0529@users.noreply.github.com>
2025-11-10 22:41:30 +0900

    Update 다영.java

--- level 1/실패율/다영.java
+++ level 1/실패율/다영.java
@@ -34,3 +34,39 @@ class Solution {
         return answer;
     }
 }
+
+
+// GPT 개선버전 추가
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        double[] notClear = new double[N];
+        int reach = stages.length;
+        Map<Integer, Double> fail = new HashMap<>();
+        
+        // 각 스테이지별 도전자 수 카운트
+        for (int stage : stages) {
+            if (stage <= N) {
+                notClear[stage - 1]++;
+            }
+        }
+
+        // 실패율 계산
+        for (int i = 0; i < N; i++) {
+            if (reach == 0) {
+                fail.put(i + 1, 0.0);
+            } else {
+                fail.put(i + 1, notClear[i] / reach);
+                reach -= notClear[i];
+            }
+        }
+
+        // 실패율 기준 내림차순 정렬 + key만 추출
+        return fail.entrySet().stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .mapToInt(Map.Entry::getKey)
+                .toArray();
+    }
+}

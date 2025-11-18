dafdbe1c98a36db31b9142dec1c329fe898f0f7c
daykim <45174177+ekdud0529@users.noreply.github.com>
2025-11-18 22:17:22 +0900

    다영 11/19

---
+++ level 1/완주하지 못한 선수/다영.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String p : participant) {
+            map.put(p,  map.getOrDefault(p, 0) + 1);
+        }
+        for(String c : completion) {
+           map.put(c, map.get(c) - 1);
+        }
+        for (String parti : map.keySet()) {
+            if (map.get(parti) > 0) {
+                answer = parti;
+                break;
+            }
+        }
+        return answer;
+    }
+}
